1f1a0aacf44c1753373408ffb29ee1f21d23080b
Buybackoff <vbaybekov@gmail.com>
2014-06-22 07:21:13 -0400

    Store typings

---
+++ WebHost/FrontEnd/typings/server.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="..\..\..\contracts\ServiceModels\Todos.cs.d.ts" />
+/// <reference path="..\..\..\contracts\ServiceModels\Yos.cs.d.ts" />
+/// <reference path="..\..\..\contracts\Models\SampleModel.cs.d.ts" />
---
+++ WebHost/FrontEnd/typings/tsd.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="es6-promises/es6-promises.d.ts" />
+/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="signalr/signalr.d.ts" />
+
