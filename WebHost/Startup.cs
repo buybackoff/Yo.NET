0eef9e70ac3254966b99bad9a7866bacaa3791e2
Buybackoff <vbaybekov@gmail.com>
2014-06-23 10:02:02 -0400

    Disable websockets temporary to solve HttpContextBase.Response issue when used with websockets

--- WebHost/Startup.cs
+++ WebHost/Startup.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Transports;
 using Microsoft.Owin;
 using Owin;
 using ServiceImplementations;
@@ -13,6 +14,8 @@ namespace WebHost {
             // CustomUserIdProvider depends on Request indirectly via SessionFeature TODO check this
             GlobalHost.DependencyResolver.Register(typeof(IUserIdProvider), () => new CustomUserIdProvider());
             // Any connection or hub wire up and configuration should go here
+            var transportManager = GlobalHost.DependencyResolver.Resolve<ITransportManager>() as TransportManager;
+            transportManager.Remove("websockets");
             app.MapSignalR();
             
         }
